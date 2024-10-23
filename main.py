545e1fadf672c79a2d18a276b1031680319a2216
zer0warm <dqhieu.2016@gmail.com>
2024-10-23 18:52:45 +0700

    Update main.py: count characters occurrences
    Count every characters, not just those in the alphabets!
    Printed the raw count data structure on screen!

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def count_words(book):
     return len(book.split())
 
+def count_characters(book):
+    normalized = book.lower()
+    occurrences = {}
+    for c in set(normalized):
+        occurrences.update({c: normalized.count(c)})
+    return occurrences
+
 with open('./books/frankenstein.txt') as f:
     book = f.read()
     print(book)
+
     print('The book contains', count_words(book), 'words.')
+
+    occurrences = count_characters(book)
+    print(occurrences)
