0f7db5aa95286d362e3e24f8cb3cb0e93af7a8ba
zer0warm <dqhieu.2016@gmail.com>
2024-10-23 18:34:08 +0700

    Update main.py: count words of "Frankenstein" book
    The book contains 77986 words!

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def count_words(book):
+    return len(book.split())
+
 with open('./books/frankenstein.txt') as f:
     book = f.read()
     print(book)
+    print('The book contains', count_words(book), 'words.')
