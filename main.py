8759c0278b50b3f4dd3d6975789ce083eb742c1e
zer0warm <dqhieu.2016@gmail.com>
2024-10-24 00:29:15 +0700

    Refactor main.py: add comments
    Also rename ``count_characters()'' to match what it does.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def count_words(book):
+    # str.split() without argument split on any whitespace character, like
+    # space, newline or tab
     return len(book.split())
 
-def count_characters(book):
+# 1. Lowercase everything possible -> normalized content
+# 2. Use a dictionary to store the distinct characters (by using set()) and
+#    their respective counts
+# 3. The result is an arbitrarily ordered dictionary
+def count_characters_occurrences(book):
     normalized = book.lower()
     occurrences = {}
     for c in set(normalized):
@@ -9,6 +15,7 @@ def count_characters(book):
     return occurrences
 
 def report_analysis(book, book_name):
+    # report header
     print('--- BEGIN BOOK ANALYSIS REPORT ---')
 
     print('Book:', book_name)
@@ -16,10 +23,18 @@ def report_analysis(book, book_name):
     print('Word analysis:', count_words(book), 'words.')
 
     print('Character analysis:')
-    occurrences = count_characters(book)
+    occurrences = count_characters_occurrences(book)
+    # sort the characters (the dictionary keys) order based on their count (the
+    # dictionary values), and because sorted() always use ascending order,
+    # reverse it to get the largest count first
     for c in sorted(occurrences, key=occurrences.get, reverse=True):
+        # repr() gets the representation of the string:
+        # - Newline -> '\n'
+        # - Single quote is double quoted ("'")
+        # - Double quote is single quoted ('"')
         print(f"{repr(c)} occurred {occurrences.get(c)} times.")
 
+    # report footer
     print('--- END OF REPORT ---')
 
 with open('./books/frankenstein.txt') as f:
